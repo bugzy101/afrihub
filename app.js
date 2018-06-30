28ec516d60ff0e7972c09e646195c323fa8d5c50
Henry <bugzy231@gmail.com>
2018-06-30 14:16:19 -0400

    search debug

--- app.js
+++ app.js
@@ -203,7 +203,7 @@ var perPage = 12
 		$meta: "textScore"
 	      }
 	    }
-	  }).toArray(function(err, posts) {
+	  }).exec(function(err, posts)  {
 	    res.render('posts', {
 			posts: posts,
 			current: page,
